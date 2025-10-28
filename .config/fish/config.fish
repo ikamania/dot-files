f98f349eabc7fcde6d32e3e79fd39e5c60220b41
Ika Mania <maniaika07@gmail.com>
2025-10-28 18:35:31 +0400

    feat: add Fish, Kitty configuration files

---
+++ .config/fish/config.fish
@@ -0,0 +1,16 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_greeting
+end
+
+fish_vi_key_bindings
+
+alias config='git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
+alias fishconf='v .config/fish/config.fish'
+alias kittyconf='v .config/kitty/kitty.conf'
+
+alias v=nvim
+alias s="xinput set-prop 'USB Optical Mouse' 'libinput Accel Speed' -0.8"
